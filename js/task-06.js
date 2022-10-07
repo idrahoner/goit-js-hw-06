042ec2c6117e19a4986048a6b97d5b3e97dc107e
Ivan Drahoner <idrahoner@gmail.com>
2022-10-07 15:59:42 +0300

    Edited 6th task

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,16 @@
+// Напиши скрипт, який під час втрати фокусу на інпуті(подія blur),
+//   перевіряє його вміст щодо правильної кількості введених символів.
+
+//  - Яка кількість смиволів повинна бути в інпуті,
+//    зазначається в його атрибуті data - length.
+//  - Якщо введена правильна кількість символів, то border інпуту стає зеленим,
+//    якщо неправильна кількість - червоним.
+//  - Для додавання стилів використовуй CSS - класи valid і invalid,
+//    які ми вже додали у вихідні файли завдання.
+
 function borderColorSwitcher({ element, validClass, invalidClass } = {}) {
   let inputValue = 0;
+  const requestedNumber = Number(element.dataset.length);
   const removeClass = () => {
     element.classList.remove(validClass);
     element.classList.remove(invalidClass);
@@ -14,7 +25,7 @@ function borderColorSwitcher({ element, validClass, invalidClass } = {}) {
   element.addEventListener("blur", () => {
     if (inputValue === 0) {
       removeClass();
-    } else if (inputValue === 6) {
+    } else if (inputValue === requestedNumber) {
       element.classList.add(validClass);
     } else {
       element.classList.add(invalidClass);
