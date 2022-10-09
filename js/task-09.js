2da097dc08101d97880b42cabbe786e6224fddfb
Ivan Drahoner <idrahoner@gmail.com>
2022-10-09 12:37:43 +0300

    Edited 9th task

--- js/task-09.js
+++ js/task-09.js
@@ -3,7 +3,9 @@
 //  - Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const body = document.body;
