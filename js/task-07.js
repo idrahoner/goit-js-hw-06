e1ae748bd7457bfca1e721a475a70d2c911952d7
Ivan Drahoner <idrahoner@gmail.com>
2022-10-09 22:35:29 +0300

    Last check before hacding in homework

--- js/task-07.js
+++ js/task-07.js
@@ -5,6 +5,8 @@
 const sliderEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
+textEl.style.fontSize = sliderEl.value + "px";
+
 sliderEl.addEventListener("input", (event) => {
   textEl.style.fontSize = event.currentTarget.value + "px";
 });
